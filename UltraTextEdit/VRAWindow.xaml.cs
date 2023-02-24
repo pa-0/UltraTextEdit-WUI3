b77a80df45d8634a48d53d40ce028f183125b9e6
jpb <jakub.baranski.33@gmail.com>
2023-02-24 19:47:32 +0000

    Fix the Version Reporter Applet

--- UltraTextEdit/VRAWindow.xaml.cs
+++ UltraTextEdit/VRAWindow.xaml.cs
@@ -15,108 +15,40 @@ using Windows.Foundation.Collections;
 using WinUICommunity.Common.Helpers;
 using System.Runtime.InteropServices; // For DllImport
 using WinRT; // required to support Window.As<ICompositionSupportsSystemBackdrop>()
+using Microsoft.Extensions.Configuration;
+using Microsoft.UI.Composition.SystemBackdrops;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace UltraTextEdit;
 
-class WindowsSystemDispatcherQueueHelper
-{
-    [StructLayout(LayoutKind.Sequential)]
-    struct DispatcherQueueOptions
-    {
-        internal int dwSize;
-        internal int threadType;
-        internal int apartmentType;
-    }
-
-    [DllImport("CoreMessaging.dll")]
-    private static unsafe extern int CreateDispatcherQueueController(DispatcherQueueOptions options, IntPtr* instance);
-
-    IntPtr m_dispatcherQueueController = IntPtr.Zero;
-    public void EnsureWindowsSystemDispatcherQueueController()
-    {
-        if (Windows.System.DispatcherQueue.GetForCurrentThread() != null)
-        {
-            // one already exists, so we'll just use it.
-            return;
-        }
-
-        if (m_dispatcherQueueController == IntPtr.Zero)
-        {
-            DispatcherQueueOptions options;
-            options.dwSize = Marshal.SizeOf(typeof(DispatcherQueueOptions));
-            options.threadType = 2;    // DQTYPE_THREAD_CURRENT
-            options.apartmentType = 2; // DQTAT_COM_STA
-
-            unsafe
-            {
-                IntPtr dispatcherQueueController;
-                CreateDispatcherQueueController(options, &dispatcherQueueController);
-                m_dispatcherQueueController = dispatcherQueueController;
-            }
-        }
-    }
-}
 /// <summary>
 /// An empty window that can be used on its own or navigated to within a Frame.
 /// </summary>
 public sealed partial class VRAWindow : Window
 {
+
+    WindowsSystemDispatcherQueueHelper m_wsdqHelper; // See below for implementation.
+    MicaController m_backdropController;
+    SystemBackdropConfiguration m_configurationSource;
+
     public VRAWindow()
     {
         this.InitializeComponent();
-        m_wsdqHelper = new WindowsSystemDispatcherQueueHelper();
-        m_wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
-
-        SetBackdrop(BackdropType.MicaAlt);
+        this.Title = "Version Reporter Applet";
+        TrySetSystemBackdrop();
     }
 
-    public enum BackdropType
-    {
-        Mica,
-        MicaAlt,
-        DesktopAcrylic,
-        DefaultColor,
-    }
-
-    WindowsSystemDispatcherQueueHelper m_wsdqHelper;
-    BackdropType m_currentBackdrop;
-    Microsoft.UI.Composition.SystemBackdrops.MicaController m_micaController;
-    Microsoft.UI.Composition.SystemBackdrops.DesktopAcrylicController m_acrylicController;
-    Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration m_configurationSource;
-
-    public void SetBackdrop(BackdropType type)
-    {
-        // Reset to default color. If the requested type is supported, we'll update to that.
-        // Note: This sample completely removes any previous controller to reset to the default
-        //       state. This is done so this sample can show what is expected to be the most
-        //       common pattern of an app simply choosing one controller type which it sets at
-        //       startup. If an app wants to toggle between Mica and Acrylic it could simply
-        //       call RemoveSystemBackdropTarget() on the old controller and then setup the new
-        //       controller, reusing any existing m_configurationSource and Activated/Closed
-        //       event handlers.
-        m_currentBackdrop = BackdropType.DefaultColor;
-        if (m_micaController != null)
-        {
-            m_micaController.Dispose();
-            m_micaController = null;
-        }
-        if (m_acrylicController != null)
-        {
-            m_acrylicController.Dispose();
-            m_acrylicController = null;
-        }
-        m_configurationSource = null;
-    }
-
-    bool TrySetMicaBackdrop(bool useMicaAlt)
+    bool TrySetSystemBackdrop()
     {
         if (Microsoft.UI.Composition.SystemBackdrops.MicaController.IsSupported())
         {
-            // Hooking up the policy object
-            m_configurationSource = new Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration();
+            m_wsdqHelper = new WindowsSystemDispatcherQueueHelper();
+            m_wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
+
+            // Create the policy object.
+            m_configurationSource = new SystemBackdropConfiguration();
             this.Activated += Window_Activated;
             this.Closed += Window_Closed;
             ((FrameworkElement)this.Content).ActualThemeChanged += Window_ThemeChanged;
@@ -125,51 +57,19 @@ public sealed partial class VRAWindow : Window
             m_configurationSource.IsInputActive = true;
             SetConfigurationSourceTheme();
 
-            m_micaController = new Microsoft.UI.Composition.SystemBackdrops.MicaController();
-
-            if (useMicaAlt)
+            m_backdropController = new MicaController()
             {
-                m_micaController.Kind = Microsoft.UI.Composition.SystemBackdrops.MicaKind.BaseAlt;
-            }
-            else
-            {
-                m_micaController.Kind = Microsoft.UI.Composition.SystemBackdrops.MicaKind.Base;
-            }
+                Kind = MicaKind.BaseAlt
+            };
 
             // Enable the system backdrop.
             // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
-            m_micaController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
-            m_micaController.SetSystemBackdropConfiguration(m_configurationSource);
+            m_backdropController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+            m_backdropController.SetSystemBackdropConfiguration(m_configurationSource);
             return true; // succeeded
         }
 
         return false; // Mica is not supported on this system
-    }
-
-    bool TrySetAcrylicBackdrop()
-    {
-        if (Microsoft.UI.Composition.SystemBackdrops.DesktopAcrylicController.IsSupported())
-        {
-            // Hooking up the policy object
-            m_configurationSource = new Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration();
-            this.Activated += Window_Activated;
-            this.Closed += Window_Closed;
-            ((FrameworkElement)this.Content).ActualThemeChanged += Window_ThemeChanged;
-
-            // Initial configuration state.
-            m_configurationSource.IsInputActive = true;
-            SetConfigurationSourceTheme();
-
-            m_acrylicController = new Microsoft.UI.Composition.SystemBackdrops.DesktopAcrylicController();
-
-            // Enable the system backdrop.
-            // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
-            m_acrylicController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
-            m_acrylicController.SetSystemBackdropConfiguration(m_configurationSource);
-            return true; // succeeded
-        }
-
-        return false; // Acrylic is not supported on this system
     }
 
     private void Window_Activated(object sender, WindowActivatedEventArgs args)
@@ -179,17 +79,12 @@ public sealed partial class VRAWindow : Window
 
     private void Window_Closed(object sender, WindowEventArgs args)
     {
-        // Make sure any Mica/Acrylic controller is disposed so it doesn't try to
-        // use this closed window.
-        if (m_micaController != null)
+        // Make sure any Mica/Acrylic controller is disposed
+        // so it doesn't try to use this closed window.
+        if (m_backdropController != null)
         {
-            m_micaController.Dispose();
-            m_micaController = null;
-        }
-        if (m_acrylicController != null)
-        {
-            m_acrylicController.Dispose();
-            m_acrylicController = null;
+            m_backdropController.Dispose();
+            m_backdropController = null;
         }
         this.Activated -= Window_Activated;
         m_configurationSource = null;
@@ -213,9 +108,42 @@ public sealed partial class VRAWindow : Window
         }
     }
 
-
     private void CopyButton_Click(object sender, RoutedEventArgs e)
     {
 
+    }
+}
+
+class WindowsSystemDispatcherQueueHelper
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct DispatcherQueueOptions
+    {
+        internal int dwSize;
+        internal int threadType;
+        internal int apartmentType;
+    }
+
+    [DllImport("CoreMessaging.dll")]
+    private static extern int CreateDispatcherQueueController([In] DispatcherQueueOptions options, [In, Out, MarshalAs(UnmanagedType.IUnknown)] ref object dispatcherQueueController);
+
+    object m_dispatcherQueueController = null;
+    public void EnsureWindowsSystemDispatcherQueueController()
+    {
+        if (Windows.System.DispatcherQueue.GetForCurrentThread() != null)
+        {
+            // one already exists, so we'll just use it.
+            return;
+        }
+
+        if (m_dispatcherQueueController == null)
+        {
+            DispatcherQueueOptions options;
+            options.dwSize = Marshal.SizeOf(typeof(DispatcherQueueOptions));
+            options.threadType = 2;    // DQTYPE_THREAD_CURRENT
+            options.apartmentType = 2; // DQTAT_COM_STA
+
+            CreateDispatcherQueueController(options, ref m_dispatcherQueueController);
+        }
     }
 }
